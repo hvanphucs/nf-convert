e54f34226b32ae5a766a69a3b6323d1db74a66d2
hvanphucs <47340777+hvanphucs@users.noreply.github.com>
2023-07-25 13:22:41 +0700

    fix UI

--- convert-nf.py
+++ convert-nf.py
@@ -77,10 +77,16 @@ def main():
 
         node_params = node.get('app_data', {})
         for (key, value) in node_params.items():
-            # print(f'{key}={value}')
+            print(f'{key}={value}')
             pass
 
-        node_label = "step" + node_id[0:3] + node_params.get('id', 'UnLabeled')
+        node_label = node.get('label', None)
+        if node_label == None:
+            node_label = "step_" + node_id[0:3] + \
+                node_params.get('id', 'UnLabeled')
+        else:
+            node_label = "step_" + node_id[0:3] + \
+                node_label
 
         node_name = to_camel_case(node_label)
         node_filename = node_params.get('filename', None)
