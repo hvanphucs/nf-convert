e7fc0eb31de89e8faafbe5946352897a3bf49bb3
hvanphucs <47340777+hvanphucs@users.noreply.github.com>
2023-07-26 12:47:24 +0700

    fix UI

--- convert-nf.py
+++ convert-nf.py
@@ -203,7 +203,7 @@ def main():
                     papermill \
                         -k python3 \
                         --log-output --log-level DEBUG \
-                        --cwd {params.output_dir} \
+                        --cwd $pwd \
                         --request-save-on-cell-execute \
                         --autosave-cell-every 10 \
                         --progress-bar \
