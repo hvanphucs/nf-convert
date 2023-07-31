2c5743fbfec1886275a2c5a636fab94d41cc3da7
hvanphucs <47340777+hvanphucs@users.noreply.github.com>
2023-07-31 16:28:06 +0700

    fix UI

---
+++ scripts/db.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import json
+import os
+
+
+def update_db(run_config, db_file):
+    current_db = []
+    existOne = False
+
+    parent = os.path.dirname(db_file)
+    if not os.path.exists(parent):
+        os.makedirs(parent)
+
+    try:
+        with open(db_file, "r") as f:
+            json.load(f)
+
+        for run in current_db:
+            if run["run_id"] == run_config["run_id"]:
+                existOne = True
+                break
+    except:
+        pass
+
+    if not existOne:
+        current_db.append(run_config)
+    with open(db_file, "r") as f:
+        json.load(f, current_db, indent=4)
